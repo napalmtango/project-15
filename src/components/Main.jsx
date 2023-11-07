2f69a38577b0d1e4d48e12426b372d2f62a1698d
John Bush <142678424+napalmtango@users.noreply.github.com>
2023-11-07 04:16:01 -0800

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import Choices from "./Choices";
 const copy0 = 'Welcome to "Do you Feel Lucky?."';
 
 const copy1 =
-  "You press the button, and we'll serve up a random joke that only a coder could love ;)";
+  "Press the button, and we'll serve up a random joke that only a coder could love ;)";
 
 const copy2 = "You can also choose from knock-knock and general jokes.";
 
