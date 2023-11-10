785264ec9136e3b4ee5d91753a7c5fc414fc4cb0
John Bush <142678424+napalmtango@users.noreply.github.com>
2023-11-10 12:53:12 -0800

    final version of jokes app before revising to Sums & Nums

---
+++ src/components/unused/Display.jsx
@@ -0,0 +1,13 @@
+import JokesField from "./JokesField.jsx";
+
+export default function Display() {
+  return (
+    <>
+      <p>
+        <div className="joke-display">
+          <JokesField />
+        </div>
+      </p>
+    </>
+  );
+}
